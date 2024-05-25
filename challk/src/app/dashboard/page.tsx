7946a29a129c6cf69a67f4b7a08429d58847665c
akxhayx <akshayeaaron30304@gmail.com>
2024-05-25 15:59:44 +0530

    db,optimized

--- challk/src/app/dashboard/page.tsx
+++ challk/src/app/dashboard/page.tsx
@@ -20,7 +20,6 @@ import {
   LineChart,
   Menu,
   Book,
-  Clipboard,
   Search,
   ShoppingCart,
   BookOpenCheck,
@@ -57,17 +56,14 @@ function Dashboard() {
         <div className="flex h-full max-h-screen flex-col gap-2 fixed">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
-              <Clipboard className="h-6 w-6" />
-              <span className="">NMIT LMS</span>
+              <div className="h-6 w-6" />
+              <span className="mr-20">NMIT LMS</span>
             </Link>
-            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-              <span className="sr-only">Toggle notifications</span>
-            </Button>
           </div>
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               <Link
-                href="/dashboarf"
+                href="/dashboard"
                 className="flex items-center gap-3 rounded-lg bg-secondary px-3 py-2 text-primary transition-all hover:text-primary"
               >
                 <Grid className="h-4 w-4" />
@@ -130,25 +126,8 @@ function Dashboard() {
       <div className="flex flex-col">
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
-            <SheetTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="shrink-0 md:hidden"
-              >
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
             <SheetContent side="left" className="flex flex-col">
               <nav className="grid gap-2 text-lg font-medium">
-                <Link
-                  href="#"
-                  className="flex items-center gap-2 text-lg font-semibold"
-                >
-                  <Clipboard className="h-6 w-6" />
-                  <span className="sr-only">Acme Inc</span>
-                </Link>
                 <Link
                   href="#"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
@@ -191,7 +170,7 @@ function Dashboard() {
                   href="#"
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                 >
-                  <BookOpenCheck className="h-5 w-5" />
+                  <Book className="h-5 w-5" />
                   CIE
                 </Link>
               </nav>
