e40827d17b4350690086829f70229f5289a9b8ea
1Aditya7 <aditya17parthiban@gmail.com>
2024-05-25 11:59:39 +0530

    UI Optimisations

--- challk/src/app/courses/[courseId]/page.tsx
+++ challk/src/app/courses/[courseId]/page.tsx
@@ -45,7 +45,7 @@ export default function CourseDetails() {
             <CardTitle className="group flex items-center gap-2 text-lg">
               Design and Analysis of Algorithms
             </CardTitle>
-            <CardDescription>Course Instructor: Ms. Roopa</CardDescription>
+            <CardDescription>Course Instructor: Ms. Algo Expert</CardDescription>
           </div>
           <div className="ml-auto flex items-center gap-1">
             <DropdownMenu>
@@ -67,7 +67,6 @@ export default function CourseDetails() {
               <ul className="grid gap-3">
                 <li className="flex items-center justify-between">
                   <span className="text-muted-foreground flex flex-row">
-                  <Calendar className='h-4 w-4'></Calendar>
                   <Button variant="link" className='text-muted-foreground hover:text-primary'>Academic Calendar</Button>
                   </span>
                 </li>
@@ -217,21 +216,13 @@ export default function CourseDetails() {
         </CardContent>
         <CardFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
           <div className="text-xs text-muted-foreground">
-            Updated <time dateTime="2023-11-23">November 23, 2023</time>
+            Updated: <time dateTime="2024-05-25">Saturday 25 May, 2024</time>
           </div>
           <Pagination className="ml-auto mr-0 w-auto">
             <PaginationContent>
               <PaginationItem>
-                <Button size="icon" variant="outline" className="h-6 w-6">
-                  <ChevronLeft className="h-3.5 w-3.5" />
-                  <span className="sr-only">Previous Order</span>
-                </Button>
               </PaginationItem>
               <PaginationItem>
-                <Button size="icon" variant="outline" className="h-6 w-6">
-                  <ChevronRight className="h-3.5 w-3.5" />
-                  <span className="sr-only">Next Order</span>
-                </Button>
               </PaginationItem>
             </PaginationContent>
           </Pagination>
